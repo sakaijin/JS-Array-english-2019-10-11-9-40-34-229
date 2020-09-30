74912fcd7fce0bd2c5f3f1e160f19d12b2355d26
Erik <erik.daniel.nobleta@oocl.com>
2020-09-30 12:20:03 +0800

    Completed index.js

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
-//TODO
+console.log(typeof(a));
+console.log(typeof(b));
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
-// TODO should output [2,4,6,8,10]
-
+for (var x = 0; x < 5; x++){
+    a[x] = a[x] * 2;
+}
+console.log(a);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
-//TODO case 1 output: 'Red Green White Black'
-// case 2 output: 'Red+Green+White+Black'
-// case 3 output: 'Red,Green,White,Black'
+console.log(colors[0] + " " + colors [1] + " " + colors[2] + " " + colors [3]);
+console.log(colors[0] + "+" + colors [1] + "+" + colors[2] + "+" + colors [3]);
+console.log(colors[0] + "," + colors [1] + "," + colors[2] + "," + colors [3]);
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
-//TODO should output: [10,8,5,4,1]
-
+a.sort((a,b)=>b-a);
+console.log(a);
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
-//TODO should output: 'a'
+var frequency = 1;
+var counter = 0;
+var value;
+for (var x = 0; x < 13; x++){
+    for (var y = x; y < 13; y++){
+        if (a[x] === a[y]) {
+            counter++;
+        }
+        if (frequency<counter){
+            frequency=counter;
+            value = a[x];
+        }
+    }
+    counter=0;
+}
+console.log(value);
